1b6c09c68237b8033d00915d115ebdf67126d408
Justin Konecny <konecnyjustin@gmail.com>
2018-08-17 11:19:22 -0400

    Update Javadoc

--- search/Advertisement.java
+++ search/Advertisement.java
@@ -4,16 +4,29 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents one vehicle listing. A listing includes a title, location, url, description, age (days since posting),
+ * list of images, and attributes (descriptors).
+ */
 public class Advertisement {
-
+    //the listing title
     private String title;
+    //the listing location (e.g. "newyork")
     private String location;
+    //the url to the online advertisement
     private String link;
+    //the description of the vehicles attributes (e.g. miles, vin, color)
     private String attributes;
+    //the number of days since posted
     private int age;
+    //the listings list of images
     private List<Image> imageList;
+    //the listing's provided description
     private String body;
 
+    /**
+     * Constructs an Advertisement and initializes the fields (i.e. title = "title")
+     */
     public Advertisement() {
         this.title = "title";
         this.location = "location";
@@ -24,58 +37,128 @@ public class Advertisement {
         this.imageList = new ArrayList<Image>();
     }
 
+    /**
+     * Set's this Advertisment's title to the given value
+     *
+     * @param title the new title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Sets this Advertisement's location to the given value
+     *
+     * @param location the new location
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * Sets this Advertisement's age to the given value
+     *
+     * @param age the number of days since posted
+     */
     public void setAge(int age) {
         this.age = age;
     }
 
+    /**
+     * Sets this Advertisement's URL to the given value
+     *
+     * @param link the new URL
+     */
     public void setLink(String link) {
         this.link = link;
     }
 
+    /**
+     * Sets this Advertisements attribute description to the given value
+     *
+     * @param attributes the new description of attributes
+     */
     public void setAttributes(String attributes) {
         this.attributes = attributes;
     }
 
+    /**
+     * Sets this Advertisement's list of images to the given list
+     *
+     * @param imageList the new list of images
+     */
     public void setImages(List<Image> imageList) {
         this.imageList = imageList;
     }
 
+    /**
+     * Set's this Advertisement's description to the given value
+     *
+     * @param body the new body/description
+     */
     public void setBody(String body) {
         this.body = body;
     }
 
+    /**
+     * Returns this Advertisement's title
+     *
+     * @return the title
+     */
     public String getTitle() {
         return this.title;
     }
 
+    /**
+     * Returns this Advertisement's location
+     *
+     * @return the location
+     */
     public String getLocation() {
         return this.location;
     }
 
+    /**
+     * Returns this advertisement's URL
+     *
+     * @return the URL
+     */
     public String getLink() {
         return this.link;
     }
 
+    /**
+     * Returns this Advertisement's attribute description
+     *
+     * @return the attributes
+     */
     public String getAttributes() {
         return this.attributes;
     }
 
+    /**
+     * Returns the number of days since this Advertisement was posted
+     *
+     * @return the age
+     */
     public int getAge() {
         return this.age;
     }
 
+    /**
+     * Returns this Advertisement's list of images
+     *
+     * @return the list of images
+     */
     public List<Image> getImages() {
         return this.imageList;
     }
 
+    /**
+     * Returns this Advertisements's description
+     *
+     * @return the body/description
+     */
     public String getBody() {
         return this.body;
     }
